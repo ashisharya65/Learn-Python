ac61b3df95cdba14d3507252f6d071d338e9db90
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-12-22 07:06:02 +0000

    Adding More files

---
+++ Programs/01.HelloWorld.py
@@ -0,0 +1,6 @@
+
+# input function to prompt the user for providing the name
+name = input("Enter your name: ")   
+
+# Using F-Strings to interpolate name variable value into the string.
+print(f"Hello! {name}, Welcome to Python Programming Tutorials.") 
