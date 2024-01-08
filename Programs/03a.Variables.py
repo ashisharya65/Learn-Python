cb6dc8d99511efa1cbc0de72690b93c7724ae5d5
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-01-08 17:41:32 +0000

    More Files

---
+++ Programs/03a.Variables.py
@@ -0,0 +1,17 @@
+
+print("\n########################################################")
+print("This is a script explaining about variables in Python!")
+print("########################################################\n")
+
+fname = "Ashish"
+lname = "Arya"
+age = 38
+
+print(f"First variable : {fname} \nSecond variable : {lname} \nThird variable : {age}")
+
+print("\nUse of type() function is to get the type of variable: ")
+x = "Karan"
+y = 20
+print(f"Type of variable x : {type(x)} \ntype of variable y : {type(y)}\n")
+
+            
