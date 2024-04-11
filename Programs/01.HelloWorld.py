8e2eb5e0a585d834e91eec449da4ca98b876ff78
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-04-11 12:12:47 +0530

    Delete Programs directory

--- Programs/01.HelloWorld.py
+++
@@ -1,6 +0,0 @@
-
-# input function to prompt the user for providing the name
-name = input("Enter your name: ")   
-
-# Using F-Strings to interpolate name variable value into the string.
-print(f"Hello! {name}, Welcome to Python Programming Tutorials.") 
